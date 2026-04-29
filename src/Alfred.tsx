b76cd97158be42a81b5b431cc961ca98bf075590
Alfred Manuel <alfred.manuel@estudiants.urv.cat>
2026-04-29 19:51:28 +0200

    added abtme

--- src/Alfred.tsx
+++ src/Alfred.tsx
@@ -4,7 +4,22 @@ function App() {
 
   return (
     <div>
-      <h1>Hola Web!</h1>
+      
+      <h1>Apuntes y Writeups de Hacking</h1>
+
+      <p>
+      Esta es una web personal donde documento mi aprendizaje en ciberseguridad.
+      Aquí recopilo apuntes, writeups de CTFs y laboratorios, así como herramientas,
+      comandos y metodologías que utilizo en hacking ético.
+      </p>
+
+      <p>
+      El objetivo es organizar mi conocimiento, seguir mi progreso y compartirlo
+      con otros.
+      </p>
+
+      <p><strong>⚠️ Solo con fines educativos.</strong></p>
+
     </div>
   )
 
