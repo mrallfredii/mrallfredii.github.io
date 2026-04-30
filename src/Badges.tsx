0e5da23920cdd5b6f34e3f5209e6d7f6f8d8f8a0
Alfred Manuel <alfred.manuel@estudiants.urv.cat>
2026-04-30 14:13:04 +0200

    added insignias

--- src/Badges.tsx
+++ src/Badges.tsx
@@ -1,14 +1,30 @@
+import './Badges.css'
 
 const Badges = () => {
   return (
-    <div>
+    <div className="badges">
+        <br />
         <br />
         <hr></hr>
-        <h1>Badges</h1>
+        <h1>Insignias 🏆</h1>
         <br />
-        <a href="https://tryhackme.com/p/mrallfredii" target="_blank">
-            <img src="https://tryhackme-badges.s3.amazonaws.com/mrallfredii.png" width={300} height={80}/>
-        </a>
+        <br />
+        <div className='links'>
+          <a href="https://tryhackme.com/p/mrallfredii" target="_blank">
+              <img src="https://tryhackme-badges.s3.amazonaws.com/mrallfredii.png" width={300} height={80}/>
+          </a>
+          <br />
+          <br />
+
+          <a href="https://tryhackme.com/p/mrallfredii" target="_blank">
+            <img src="./src/assets/thm.png" width={300} height={160}/>
+          </a>
+
+          <a href="https://profile.hackthebox.com/profile/019daa3e-ef99-718b-9f2e-dc6b621b5d3b" target="_blank">
+            <img src="./src/assets/htb.png" width={300} height={160}/>
+          </a>
+        </div>
+
         <br />
         <br />
     </div>
